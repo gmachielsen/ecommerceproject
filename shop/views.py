ee944d9d2d8883c1707605ad9f0e099e2a4665fb
gmachielsen <g.machielsen@gmail.com>
2020-01-05 16:19:47 +0100

    initial commit

--- shop/views.py
+++ shop/views.py
@@ -11,14 +11,14 @@ def allProdCat(request, c_slug=None):
     products = None
     if c_slug!=None:
         c_page = get_object_or_404(Category, slug=c_slug)
-        products = Product.objects.filter(Category=c_page, available=True)
+        products = Product.objects.filter(category=c_page, available=True)
     else:
         products = Product.objects.all().filter(available=True)
-    return render(request, 'shop/category.html', {'category':c_page, 'products': products})
+    return render(request, 'shop/category.html', {'category':c_page,'products':products})
 # Create your views here.
 def ProdCatDetail(request, c_slug, product_slug):
     try:
         product = Product.objects.get(category__slug=c_slug, slug=product_slug)
     except Exception as e:
         raise e
-    return render(request, 'shop/product.html', {'product':product})
+    return render(request, 'shop/product.html', {'product': product})
