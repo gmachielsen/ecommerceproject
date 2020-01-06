3e0701fe0ff876febd00ce9074421ea67f0c56f3
gmachielsen <g.machielsen@gmail.com>
2020-01-06 18:07:06 +0100

    initial commit

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,7 +1,6 @@
-"""ecommerce URL Configuration
-
+"""perfectcushion URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -26,5 +25,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+	urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- shop/urls.py
+++ shop/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name='shop'
 
 urlpatterns = [
-    path('', views.allProdCat, name='allProdCat'),
-    path('<slug:c_slug>/', views.allProdCat, name='products_by_category'),
-    path('<slug:c_slug>/<slug:product_slug>/', views.ProdCatDetail, name='ProdCatDetail'),
+	path('', views.allProdCat, name='allProdCat'),
+	path('<slug:c_slug>/', views.allProdCat, name='products_by_category'),
+	path('<slug:c_slug>/<slug:product_slug>/', views.ProdCatDetail, name='ProdCatDetail'),
 ]
